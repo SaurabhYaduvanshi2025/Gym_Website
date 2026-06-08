4015de27b8d82dbd33baf8434bb7ffcb42be31e1
Saurabh Yaduvanshi <saurabhyaduvanshi2018@gmail.com>
2026-06-08 10:55:31 +0530

    Update majore files

---
+++ include/science.php
@@ -0,0 +1,73 @@
+<section class="features-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="featured-columns-wrap">
+              <div class="featured-items-style2 bg-img" data-bg-img="assets/img/photos/bg-f2.jpg">
+                <div class="row row-gutter-0">
+                  <div class="col-md-6">
+                    <!-- Start Featured Item -->
+                    <div class="featured-item">
+                      <div class="featured-icon">
+                        <img src="assets/img/icons/f1.png" alt="Image">
+                      </div>
+                      <div class="featured-info">
+                        <h4>Best Training</h4>
+                        <p>Best Training dolor sit consectetur adipiscing elit, sed do eiusmod temp incididunt ut labore et dolore</p>
+                      </div>
+                    </div>
+                    <!-- End Featured Item -->
+                  </div>
+                  <div class="col-md-6">
+                    <!-- Start Featured Item -->
+                    <div class="featured-item">
+                      <div class="featured-icon">
+                        <img src="assets/img/icons/f2.png" alt="Image">
+                      </div>
+                      <div class="featured-info">
+                        <h4>Qualified Instructor</h4>
+                        <p>Qualified Instructor consectetur adipg elit, sed do eiusmod tempor incididu ut labore dolore are same magna </p>
+                      </div>
+                    </div>
+                    <!-- End Featured Item -->
+                  </div>
+                  <div class="col-md-6">
+                    <!-- Start Featured Item -->
+                    <div class="featured-item">
+                      <div class="featured-icon">
+                        <img src="assets/img/icons/f3.png" alt="Image">
+                      </div>
+                      <div class="featured-info">
+                        <h4>Latest Equipment</h4>
+                        <p class="m-0">Latest equipment dolor consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+                      </div>
+                    </div>
+                    <!-- End Featured Item -->
+                  </div>
+                  <div class="col-md-6">
+                    <!-- Start Featured Item -->
+                    <div class="featured-item">
+                      <div class="featured-icon">
+                        <img src="assets/img/icons/f4.png" alt="Image">
+                      </div>
+                      <div class="featured-info">
+                        <h4>Award Winners</h4>
+                        <p class="m-0">Award winner dolor sit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna </p>
+                      </div>
+                    </div>
+                    <!-- End Featured Item -->
+                  </div>
+                </div>
+              </div>
+              <div class="featured-discount-item">
+                <div class="thumb" data-bg-img="assets/img/photos/f1.jpg"></div>
+                <div class="content">
+                  <h2>35%</h2>
+                  <h3>DISCOUNT</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
---
+++ include/servicearea.php
@@ -0,0 +1,169 @@
+<section class="service-area service-default-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12">
+            <div class="section-title text-center" data-aos="fade-up" data-aos-duration="1000">
+              <h2 class="title"><span>CLASSES </span>WE PROVIDE</h2>
+              <div class="desc">
+                <p>Gym classes dolor sit amet, consectetur adipiscing elit, sed do eiod tempor <br>didunt ut labore et dolore m et dolore magna aliqua minim niam</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row" data-aos="fade-up" data-aos-duration="1100">
+          <div class="col-md-10 offset-md-1 col-lg-12 offset-lg-0">
+            <div class="row">
+              <div class="col-sm-6 col-lg-4">
+                <!-- Start Service Item -->
+                <div class="service-item">
+                  <div class="inner-content">
+                    <div class="thumb">
+                      <a href="class-details.html"><span><img src="assets/img/service/1.jpg" alt="Image"></span></a>
+                    </div>
+                    <div class="content">
+                      <h4 class="title"><a href="class-details.html">BODY BUILDING CLASS</a></h4>
+                      <h4 class="class-time">Duration 50 Minutes</h4>
+                    </div>
+                  </div>
+                </div>
+                <!-- End Service Item -->
+              </div>
+              <div class="col-sm-6 col-lg-4">
+                <!-- Start Service Item -->
+                <div class="service-item">
+                  <div class="inner-content">
+                    <div class="thumb">
+                      <a href="class-details.html"><span><img src="assets/img/service/2.jpg" alt="Image"></span></a>
+                    </div>
+                    <div class="content">
+                      <h4 class="title"><a href="class-details.html">FITNESS CLASS</a></h4>
+                      <h4 class="class-time">Duration 45 Minutes</h4>
+                    </div>
+                  </div>
+                </div>
+                <!-- End Service Item -->
+              </div>
+              <div class="col-sm-6 col-lg-4">
+                <!-- Start Service Item -->
+                <div class="service-item">
+                  <div class="inner-content">
+                    <div class="thumb">
+                      <a href="class-details.html"><span><img src="assets/img/service/3.jpg" alt="Image"></span></a>
+                    </div>
+                    <div class="content">
+                      <h4 class="title"><a href="class-details.html">CROSSFIT CLASS</a></h4>
+                      <h4 class="class-time">Duration 35 Minutes</h4>
+                    </div>
+                  </div>
+                </div>
+                <!-- End Service Item -->
+              </div>
+              <div class="col-sm-6 col-lg-4">
+                <!-- Start Service Item -->
+                <div class="service-item">
+                  <div class="inner-content">
+                    <div class="thumb">
+                      <a href="class-details.html"><span><img src="assets/img/service/4.jpg" alt="Image"></span></a>
+                    </div>
+                    <div class="content">
+                      <h4 class="title"><a href="class-details.html">YOGA CLASS</a></h4>
+                      <h4 class="class-time">Duration 25 Minutes</h4>
+                    </div>
+                  </div>
+                </div>
+                <!-- End Service Item -->
+              </div>
+              <div class="col-sm-6 col-lg-4">
+                <!-- Start Service Item -->
+                <div class="service-item">
+                  <div class="inner-content">
+                    <div class="thumb">
+                      <a href="class-details.html"><span><img src="assets/img/service/5.jpg" alt="Image"></span></a>
+                    </div>
+                    <div class="content">
+                      <h4 class="title"><a href="class-details.html">MARTIAL ART CLASS</a></h4>
+                      <h4 class="class-time">Duration 30 Minutes</h4>
+                    </div>
+                  </div>
+                </div>
+                <!-- End Service Item -->
+              </div>
+              <div class="col-sm-6 col-lg-4">
+                <!-- Start Service Item -->
+                <div class="service-item">
+                  <div class="inner-content">
+                    <div class="thumb">
+                      <a href="class-details.html"><span><img src="assets/img/service/6.jpg" alt="Image"></span></a>
+                    </div>
+                    <div class="content">
+                      <h4 class="title"><a href="class-details.html">CARDIO CLASS</a></h4>
+                      <h4 class="class-time">Duration 40 Minutes</h4>
+                    </div>
+                  </div>
+                </div>
+                <!-- End Service Item -->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+
+
+    <!--== Start Service Area ==-->
+    <section class="classical-workout-area">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div class="classical-workout-wrap bg-img" data-bg-img="assets/img/photos/workout.bg2.jpg">
+              <div class="column-left bg-img" data-bg-img="assets/img/photos/workout.bg1.png" data-aos="fade-right" data-aos-duration="1000">
+                <div class="content">
+                  <h2 class="title">CLASSICAL<br>WORKOUT</h2>
+                  <p>Lorem ipsum dolor amet, consectetur adipiscing elit, sed do eiod tempor incididunt ut labore</p>
+                  <a class="btn-link" href="classes.html">VIEW SCHEDULE</a>
+                </div>
+              </div>
+              <div class="shape-style" data-aos="fade-right" data-aos-duration="1000"></div>
+              <div class="column-right">
+                <div class="workout-items">
+                  <div class="workout-item">
+                    <div class="thumb">
+                      <img src="assets/img/photos/workout1.png" alt="Image">
+                    </div>
+                    <div class="content">
+                      <h3>01.</h3>
+                    </div>
+                  </div>
+                  <div class="workout-item">
+                    <div class="thumb">
+                      <img src="assets/img/photos/workout2.png" alt="Image">
+                    </div>
+                    <div class="content">
+                      <h3>02.</h3>
+                    </div>
+                  </div>
+                  <div class="workout-item mb-0 mb-xxs-40">
+                    <div class="thumb">
+                      <img src="assets/img/photos/workout3.png" alt="Image">
+                    </div>
+                    <div class="content">
+                      <h3>03.</h3>
+                    </div>
+                  </div>
+                  <div class="workout-item mb-0">
+                    <div class="thumb">
+                      <img src="assets/img/photos/workout4.png" alt="Image">
+                    </div>
+                    <div class="content">
+                      <h3>04.</h3>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--== End Service Area ==-->
